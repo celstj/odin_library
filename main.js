b86d1584a9d38477c5575e2c83a382d160bc60c5
celj <kaelyptic@outlook.com>
2023-11-01 23:58:00 +0800

    HTML, CSS, JS skeleton base push

---
+++ main.js
@@ -0,0 +1,22 @@
+/*
+1. add function to script - take user input and store new book obj into array
+2. function loop array - display each book on page, eg.table or card
+3. add "NEW BOOK" btn that brings up form allowing users to input
+    details for new book: author, title, number of pages, whether been read
+    etc.
+4. add BTN on each book display to remove book from library
+    - need to associate DOM elements to actual bok obj eg. data-attribute
+      that correponds to the index of the library array.
+5. add BTN to each book diplay to change -read- status
+    - toggle
+*/
+
+const myLibrary = [];
+
+function Book() {
+    //constructor...
+}
+
+function addBookToLibrary() {
+    // todo
+}
